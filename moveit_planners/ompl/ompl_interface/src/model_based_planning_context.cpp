909edd9a8d20e0ca01bfceaaeea5e1548e2d8e81
Antoine <antoine.duplex@free.fr>
2022-07-20 13:19:11 +0200

    OMPL's new loader moved to its rigth place

--- moveit_planners/ompl/ompl_interface/src/model_based_planning_context.cpp
+++ moveit_planners/ompl/ompl_interface/src/model_based_planning_context.cpp
@@ -65,7 +65,7 @@
 #include "ompl/base/objectives/MaximizeMinClearanceObjective.h"
 #include <ompl/geometric/planners/prm/LazyPRM.h>
 
-#include <moveit/planning_interface/ompl_optimization_objective_loader.h>
+#include <moveit/ompl_interface/ompl_optimization_objective_loader.h>
 #include <pluginlib/class_loader.hpp>
 
 
