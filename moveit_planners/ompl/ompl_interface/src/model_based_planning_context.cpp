aa20a65059cf4d775857a69bba4868ab62d6856d
Antoine <antoine.duplex@free.fr>
2022-08-09 10:14:29 +0200

    Clean-up code without ConfigureContext and unneeded code related to simplify_solution

--- moveit_planners/ompl/ompl_interface/src/model_based_planning_context.cpp
+++ moveit_planners/ompl/ompl_interface/src/model_based_planning_context.cpp
@@ -371,8 +371,7 @@ void ompl_interface::ModelBasedPlanningContext::useConfig()
     cfg.erase(it);
   }
 
-
-  // check whether the path returned by the planner should be interpolated
+  // check whether the path returned by the planner should be simplified
   it = cfg.find("simplify_solutions");
   if (it != cfg.end())
   {
@@ -380,7 +379,7 @@ void ompl_interface::ModelBasedPlanningContext::useConfig()
     cfg.erase(it);
   }
 
-    // check whether solution paths from parallel planning should be hybridized
+  // check whether solution paths from parallel planning should be hybridized
   it = cfg.find("hybridize");
   if (it != cfg.end())
   {
